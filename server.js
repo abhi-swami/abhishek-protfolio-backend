56ba4757a2dc4a80ee589371bfa06cf422c0229f
abhi-swami <abhi-swami@gmail.com>
2026-03-12 17:37:19 +0530

    add workflows

--- server.js
+++ server.js
@@ -12,13 +12,15 @@ app.use(express.json());
 
 app.use("/", contactRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
